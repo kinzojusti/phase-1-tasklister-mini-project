9f51a43a1e43ad031bf09c62b593a57268fe0eff
+ <+>
2022-04-12 10:01:02 -0400

    saving

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  //functions 
+
+  const createNewTask = (e) => {
+    e.preventDefault();
+    // console.log('submit')
+     //grab the input 
+    const newtaskDescription = document.getElementById('new-task-description')
+    console.log(newtaskDescription.value)
+    //create li
+    const newLi = document.createElement('li')
+    console.log(newLi)
+     //print the input in the li
+    newLi.textContent = newtaskDescription.value
+    //append
+    const ul = document.querySelector("#tasks")
+    console.log(ul)
+    ul.appendChild(newLi)
+    e.target.reset();
+  }
+  
+  // grab stuff
+  const form = document.getElementById('create-task-form')
+  
+  
+  //grab event listener
+  form.addEventListener('submit', createNewTask)
+
 });
